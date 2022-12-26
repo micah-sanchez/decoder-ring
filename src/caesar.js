5cee585253e348787a68d0cf424611deb84712cc
micah-sanchez <mjsanch4@gmail.com>
2022-12-26 11:06:30 -0700

    Added logic to include non-alphabetic symbols, initial logic to push letters to result variable

--- src/caesar.js
+++ src/caesar.js
@@ -8,6 +8,56 @@ const caesarModule = (function () {
 
   function caesar(input, shift, encode = true) {
     // your solution code here
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    let result = "";
+
+    if (shift === 0 || shift > 25 || shift < -25) {
+      return false;
+    }
+
+    const lowerCaseConversion = input.toLowerCase()
+
+//create encode/decode if statement 
+    // if (encode) {
+
+    // } else if (!encode) {
+
+    // }
+
+    
+
+//loop through 
+    for (let i = 0; i < lowerCaseConversion.length; i++) {
+      const letter = lowerCaseConversion[i]
+      
+      if (!alphabet.includes(letter)) {
+        result += letter
+        
+      } else {
+        const alphabetIndex = alphabet.indexOf(letter)
+
+        if (alphabetIndex < 0) {
+          const positiveAlphabetIndex = 26 + alphabetIndex;
+          result += alphabet[positiveAlphabetIndex];
+
+        } else {
+          const shiftIndex = shift + alphabetIndex;
+          result += alphabet[shiftIndex];
+        }
+      }
+
+      
+
+      // test whether the index is negative
+      //if it is 26 + index gives index of wrap around
+      
+      
+      //console.log("input:", input[i], "alphabet index:", alphabetIndex)
+      //console.log(input, input[i])
+      // result += alphabet[shiftedAmount].toLowerCase(); 
+      //console.log(result)
+    }
+    return result
   }
 
   return {
