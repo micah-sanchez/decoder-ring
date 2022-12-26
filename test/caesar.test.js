4203cddcf521a286e7569d34c4040dbc18a4fc67
micah-sanchez <mjsanch4@gmail.com>
2022-12-26 11:06:00 -0700

    Added space test

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,1 +1,13 @@
 // Write your tests here!
+const { expect } = require("chai");
+const { caesar } = require("../src/caesar");
+
+describe("caesar() - test written by Micah -", () => {
+    it("should return a space when a space is inputted", () => {
+        const message = "i o u"
+        const expected = "j p v"
+        const actual = caesar(message, 1)
+
+        expect(actual).to.equal(expected)
+    });
+});
