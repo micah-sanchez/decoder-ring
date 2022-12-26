c1f72ca4ad2a328f52b9eeed545e4c58c21d9212
micah-sanchez <mjsanch4@gmail.com>
2022-12-26 16:15:17 -0700

    Added row and column test

--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,1 +1,21 @@
 // Write your tests here!
+const { expect } = require("chai");
+const { polybius } = require("../src/polybius");
+
+describe("polybius() - written by Micah - ", () => {
+    it("should get the row value based on the inputted letter", () => {
+        const message = 'h';
+        const expected = "2";
+        const actual = polybius(message)[1];
+
+        expect(actual).equal(expected);
+    });
+
+    it("should get the column value based on the inputted letter", () => {
+        const message = 'h';
+        const expected = "3";
+        const actual = polybius(message)[0];
+
+        expect(actual).equal(expected);
+    });
+});
